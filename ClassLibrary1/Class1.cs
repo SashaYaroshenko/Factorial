2f3486002e678ae6b7cb3970c85d8935eded1e42
SashaYaroshenko <yaroshenko2019ol@gmail.com>
2022-11-21 18:37:43 +0200

    Add VS project

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int Factorial(int n)
+    {
+        if (n < 0 || n > 12)
+        {
+            throw new Exception("ArgumentOutOfRangeException");
+        }
+        return n * Factorial(n - 1);
+    }
+}
